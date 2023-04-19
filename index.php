abc4b800cc0536b2b9c61912d831832094ffb793
Murodjon Sodikov <89474340+SADIKOVCODER@users.noreply.github.com>
2023-04-19 23:35:17 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+
+$a = rand(1111,9999);
+$b = rand(1111,9999);
+$c = rand(1111,9999);
+$d = rand(1111,9999);
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$olti = rand(111111,999999);
+$olti1 = rand(111111,999999);
+$olti2 = rand(111111,999999);
+$besh = rand(11111,99999);
+$besh1 = rand(11111,99999);
+$besh2 = rand(11111,99999);
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$uch = rand(111,999);
+$uch1 = rand(111,999);
+$uch2 = rand(111,999);
+$ikki = rand(11,99);
+$ikki1 = rand(11,99);
+$ikki2 = rand(11,99);
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$a1 = ['*','+','/','-'];
+$b1 = array_rand($a1);
+$c1 = $a1[$b1];
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$a2 = ['*','+','/','-'];
+$b2 = array_rand($a2);
+$c2 = $a2[$b2];
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$a3 = ['*','+','/','-'];
+$b3 = array_rand($a3);
+$c3 = $a3[$b3];
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$a4 = ['*','+','/','-'];
+$b4 = array_rand($a4);
+$c4 = $a4[$b4];
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$a5 = ['*','+','/','-'];
+$b5 = array_rand($a5);
+$c5 = $a5[$b5];
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$a6 = ['*','+','/','-'];
+$b6 = array_rand($a6);
+$c6 = $a6[$b6];
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$at = "a = ";
+$bt = "b = ";
+$ct = "c = ";
+$dt = "d = ";
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$randoms = ["$a$c1$b$c3$c","$ikki1$c3$uch$c4$olti2","$besh$c1$uch$c3$ikki","$ikki2$c6$uch2$c4$olti2","$uch2$c5$olti2$c2$besh1","$besh2$c5$uch2$c6$a","$besh$c5$uch2$c6$d","$uch1$c5$olti2$c3$b","$uch$c4$uch2$c6$besh2","$olti1$c4$besh2$c6$c","$d$c4$olti2$c2$ikki1"];
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+$randoms1 = array_rand($randoms);
+$randoms2 = $randoms[$randoms1];
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+echo $randoms2;
+
+//Manba: t.me/ITMAKTABI1(AyTi Maktabi 1),Dasturchi: t.me/Sadiikovv
+
+?>
